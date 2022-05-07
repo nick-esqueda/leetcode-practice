a527453550b98cf1920e8edf6baa8c3b74fe1f09
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-05-07 06:49:44 -0500

    Time: 61 ms (41.26%), Space: 15.3 MB (81.35%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -24,35 +24,66 @@ class Solution:
           
 #         return d
           
-#     def maxDepth(self, root: Optional[TreeNode]) -> int:
-#         """
-#         DFT
-#               3
-#             9   20
-#            1 4    10
-#               5
-#              2 6
-        
-        
-#         sta = []
-#         depth = 5
-#         curr = 2
-#         len = 0
-#         while...
-        
-          
-#         return x
-#         """
-        
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-      """
-      if the root is none, return 0
-      otherwise...
-      take the max of the left tree and the right tree
-      return the max + 1 (for this frame) to the caller
-      """
+        """
+        DFT
+              3
+            9   20
+           1 4    10
+              5
+             2 6
+        
+        
+        sta = [(1, 3),(4, 3)]
+        maxDepth = 3
+        curr = 
+        node = 
+        currDepth = 
+        
+        while sta...
+          pop top of stack (curr)
+          node, depth = curr
+          
+          if depth > maxDepth, replace it
+          
+          if node.left, push it to the stack with the currDepth + 1
+          if node.right, push it to the stack with the currDepth + 1
+          
+        return maxDepth
+        """
+        
+        if not root:
+          return 0
+        
+        maxDepth = 1
+        sta = [(root, 1)]
+        while sta:
+          curr = sta.pop()
+          (node, depth) = curr
+          
+          if depth > maxDepth:
+            maxDepth = depth
+            
+          if node.left:
+            sta.append((node.left, depth + 1))
+          if node.right:
+            sta.append((node.right, depth + 1))
+            
+        return maxDepth
+        
+        
+        
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#       """
+#       RECURSIVE
+
+#       if the root is none, return 0
+#       otherwise...
+#       take the max of the left tree and the right tree
+#       return the max + 1 (for this frame) to the caller
+#       """
       
-      if not root:
-        return 0
+#       if not root:
+#         return 0
       
-      return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+#       return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
