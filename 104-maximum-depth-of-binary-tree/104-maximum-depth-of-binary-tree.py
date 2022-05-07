b48571ee0947a458a489de828d507c62336988e2
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-05-07 06:04:54 -0500

    Time: 85 ms (8.09%), Space: 15.5 MB (76.97%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -5,22 +5,62 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         """
+#         """
+#         if not root: return 0
+        
+#         d = 0
+#         q = [root]
+#         while q:
+#           d += 1
+#           l = len(q)
+          
+#           for _ in range(l):
+#             curr = q.pop(0)
+#             if curr.left: q.append(curr.left)
+#             if curr.right: q.append(curr.right)
+          
+#         return d
+          
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         """
+              3
+            9   20
+           1 4    10
+              5
+             2 6
+        
+        
+        sta = []
+        depth = 5
+        curr = 2
+        len = 0
+        while...
+          increment depth
+          newSta = []
+          for the length of sta...
+            pop the node (curr)
+            put the children on newSta if there are any
+            
+          sta = newSta
+          
+        return 5
         """
         
         if not root: return 0
         
+        sta = [root]
         d = 0
-        q = [root]
-        while q:
+        while sta:
           d += 1
-          l = len(q)
-          
-          for _ in range(l):
-            curr = q.pop(0)
-            if curr.left: q.append(curr.left)
-            if curr.right: q.append(curr.right)
+          newSta = []
+          for _ in range(len(sta)):
+            curr = sta.pop()
+            print(curr.val)
+            if curr.right: newSta.append(curr.right)
+            if curr.left: newSta.append(curr.left)
+              
+          sta = newSta
           
         return d
-          
