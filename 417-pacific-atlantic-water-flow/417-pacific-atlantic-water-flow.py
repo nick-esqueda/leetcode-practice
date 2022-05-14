efb19a03ce5087a9815c5b92c43f83da6e57a290
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-05-14 15:27:20 -0500

    Time: 340 ms (66.17%), Space: 15.5 MB (76.73%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -0,0 +1,43 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        """
+        declare pac_set and atl_set
+        explore() starting from cells touching the pac, and mark cells that can get to pac in pac_set
+        ^ do the same but starting from cells touching the atl, but push to atl_set
+        return whichever coords are in both sets
+        
+        explore()
+          if the cell is out of bounds, return
+          if the cell is in ONLY the corresponding set, return
+          mark the cell inside of corresponding set
+          if the top cell's val >= curr cell's val, explore the top cell
+            (do the same for bottom left right)
+          return nothing at end (only traversing to collect cell coords)
+        """
+        def explore(row, col, visited, prev_height):
+          if row < 0 or col < 0 or row >= len(heights) or col >= len(heights[0]):
+            return
+          curr_height = heights[row][col]
+          if curr_height < prev_height:
+            return
+          if (row,col) in visited:
+            return
+          
+          visited.add((row,col))
+          explore(row + 1, col, visited, curr_height)
+          explore(row - 1, col, visited, curr_height)
+          explore(row, col + 1, visited, curr_height)
+          explore(row, col - 1, visited, curr_height)
+            
+          
+        pac_set = set()
+        atl_set = set()
+        for row in range(len(heights)):
+          for col in range(len(heights[row])):
+            if row == 0 or col == 0:
+              explore(row, col, pac_set, float('-inf'))
+            if row == len(heights) - 1 or col == len(heights[0]) - 1:
+              explore(row, col, atl_set, float('-inf'))
+            
+        return list(pac_set.intersection(atl_set))
+        
