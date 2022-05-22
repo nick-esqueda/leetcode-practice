4f1fa56848c4c0c03a146fb30b5e351afc9ea421
nick-esqueda <nick_esqueda@outlook.com>
2022-05-22 14:22:43 -0500

    structy is univalue list recursive

--- structy/linked_list/is_univalue_list.py
+++ structy/linked_list/is_univalue_list.py
@@ -1,7 +1,7 @@
-# class Node:
-#   def __init__(self, val):
-#     self.val = val
-#     self.next = None
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
 
 def is_univalue_list(head):
   curr = head
@@ -11,3 +11,29 @@ def is_univalue_list(head):
       return False
     curr = curr.next
   return True
+
+
+def is_univalue_list(head, prev_val=None):
+  if head is None:
+    return True
+  
+  if prev_val is None or head.val == prev_val:
+    return is_univalue_list(head.next, head.val)
+  else:
+    return False
+  
+  
+u = Node(2)
+v = Node(2)
+w = Node(2)
+x = Node(2)
+y = Node(2)
+
+u.next = v
+v.next = w
+w.next = x
+x.next = y
+
+# 2 -> 2 -> 2 -> 2 -> 2
+
+is_univalue_list(u) # True
