a22275590809a9342a57f71884b5d303c913148b
nick-esqueda <nick_esqueda@outlook.com>
2022-05-24 12:40:48 -0500

    structy has path iterative

--- structy/graph/has_path.py
+++ structy/graph/has_path.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def has_path(graph, src, dst):
   if src == dst:
     return True
@@ -6,4 +8,15 @@ def has_path(graph, src, dst):
     if (has_path(graph, nei, dst) is True):
       return True
   
+  return False
+
+
+def has_path(graph, src, dst):
+  q = deque([src])
+  while q:
+    curr = q.popleft()
+    if curr == dst:
+      return True
+    for nei in graph[curr]:
+      q.append(nei)
   return False
