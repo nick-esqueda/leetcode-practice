54ae3bda1a3b1cb5ec3e1c9a4f0c379e77a45aa7
nick-esqueda <nick_esqueda@outlook.com>
2022-05-23 22:10:40 -0500

    structy tree levels

--- structy/binary_tree/tree_levels.py
+++ structy/binary_tree/tree_levels.py
@@ -28,3 +28,31 @@ def tree_levels(root):
     levels.append(level)
     
   return levels
+
+
+def _tree_levels(root, levels, curr_level=0):
+  """
+  pass in an array to store sub arrays where each index corresponds to the level
+  keep track of the curr level by passing in a level param (+1 on each child call)
+  if the outer array at the current index doesn't exist yet, append a new array
+    with curr.val inside (should work because you're going down level by level)
+  if an array at the curr level/idx exists, append curr.val to that sub array
+  go down left and right, passing in level + 1
+  """
+  if root is None:
+    return
+  
+  try:
+    levels[curr_level].append(root.val)
+  except:
+    levels.append([root.val])
+    
+  _tree_levels(root.left, levels, curr_level + 1)
+  _tree_levels(root.right, levels, curr_level + 1)
+  
+  
+def tree_levels(root):
+  levels = []
+  _tree_levels(root, levels)
+  return levels
+  
