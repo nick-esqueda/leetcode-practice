ededa82a7a8c9715e8ff6d61e704131187643525
nick-esqueda <nick_esqueda@outlook.com>
2022-05-23 19:18:30 -0500

    structy tree path finder recursive optimization

--- structy/binary_tree/tree_path_finder.py
+++ structy/binary_tree/tree_path_finder.py
@@ -41,3 +41,31 @@ def path_finder(root, target):
   else:
     return None
   
+
+# ^ TO DECREASE TIME COMPLEXITY FROM O(N^2):
+def _path_finder(root, target):
+  if not root:
+    return None
+  
+  if root.val == target:
+    return [root.val]
+  
+  left = _path_finder(root.left, target)
+  if left:
+    left.append(root.val)
+    return left
+  
+  right = _path_finder(root.right, target)
+  if right:
+    right.append(root.val)
+    return right
+
+  return None
+  
+
+def path_finder(root, target):
+  res = _path_finder(root, target)
+  if res is not None:
+    return res[::-1]
+  else:
+    return None
