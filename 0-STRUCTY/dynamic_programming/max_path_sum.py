01084bc9120796a6755a45d2dc29e5eaed81f115
nick-esqueda <nick_esqueda@outlook.com>
2022-05-26 15:21:03 -0500

    structy max path sum

---
+++ 0-STRUCTY/dynamic_programming/max_path_sum.py
@@ -0,0 +1,21 @@
+def max_path_sum(grid, r=0, c=0, memo={}):
+  """
+  if curr pos is out of bounds, return 0
+  if r,c is the bottom right, return the value at that pos
+  
+  pos's max path sum =
+    take the max of the right and down paths, and add curr pos's value to that
+  """
+  pos = (r, c)
+  if pos in memo:
+    return memo[pos]
+  if r not in range(len(grid)) or c not in range(len(grid[0])):
+    return 0
+  if r == len(grid) - 1 and c == len(grid[0]) - 1:
+    return grid[r][c]
+  
+  memo[pos] = (
+    max(max_path_sum(grid, r + 1, c, memo), max_path_sum(grid, r, c + 1, memo))
+        + grid[r][c]
+  )
+  return memo[pos]
