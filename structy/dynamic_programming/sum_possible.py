942c06298070eea6408d2c3201fd652c5247661c
nick-esqueda <nick_esqueda@outlook.com>
2022-05-25 21:51:36 -0500

    structy sum possible

---
+++ structy/dynamic_programming/sum_possible.py
@@ -0,0 +1,24 @@
+def sum_possible(amount, numbers, memo={}):
+  """
+  if amount == 0, return True
+  iterate through nums
+    if amount - num < 0, continue to next iteration
+    if amount - num > 0, call sum_possible(amount - num, numbers)
+      if recursion returns true, return true
+      otherwise, continue to next iteration
+  if you go through all nums, return False
+  """
+  if amount in memo:
+    return memo[amount]
+  if amount < 0:
+    return False
+  if amount == 0:
+    return True
+  
+  for num in numbers:
+    if sum_possible(amount - num, numbers, memo):
+      memo[amount] = True
+      return True
+      
+  memo[amount] = False
+  return False
