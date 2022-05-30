997c593377b4b434509250ef4ae7d2f9dce740e0
nick-esqueda <nick_esqueda@outlook.com>
2022-05-30 00:42:46 -0500

    structy quickest concat

---
+++ 0-STRUCTY/dynamic_programming/quickest_concat.py
@@ -0,0 +1,32 @@
+def quickest_concat(s, words):
+  return _quickest_concat(s, words, {})
+
+def _quickest_concat(s, words, memo):
+  """
+  need to count the length of the shortest path to word completion
+  if s is "", return 0
+  
+  loop over words:
+    take each word and see if s.startswith that word
+      if it does, get the quickest_concat of the substring without that prefix
+      compare that return val to a running min and replace if needed
+      
+  return the min val
+  """
+  if s in memo:
+    return memo[s]
+  if len(s) == 0:
+    return 0
+
+  curr_min = float('inf')
+  for word in words:
+    if s.startswith(word):
+      attempt = _quickest_concat(s[len(word):], words, memo) + 1
+      if attempt > 0:
+        curr_min = min(attempt, curr_min)
+      
+  memo[s] = curr_min if isinstance(curr_min, int) else -1
+  return memo[s]
+
+
+print(quickest_concat('caution', ['ion', 'caut', 'caution']))
