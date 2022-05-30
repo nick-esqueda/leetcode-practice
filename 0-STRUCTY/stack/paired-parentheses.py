823a2a2cfac0f9199ec276f4e8284b02c7b89ec2
nick-esqueda <nick_esqueda@outlook.com>
2022-05-30 00:57:03 -0500

    structy paired parentheses

---
+++ 0-STRUCTY/stack/paired-parentheses.py
@@ -0,0 +1,28 @@
+def paired_parentheses(string):
+  """
+  all parens must close
+  start parens must come first before a closing paren
+  the number of open and close parens must match
+  
+  if you come across an open paren, push it to a stack
+  if you come across a close paren, pop the top of the stack
+    if the stack is empty when you try to pop, return False
+  after iterating through whole string, if stack has something one it, return False
+  else, return True
+  """
+  if len(string) == 0:
+    return True
+  
+  sta = []
+  for c in string:
+    if c == "(":
+      sta.append(c)
+    elif c == ")":
+      if len(sta) == 0:
+        return False
+      else:
+        sta.pop()
+  
+  return True if len(sta) == 0 else False
+
+print(paired_parentheses("()rose(jeff"))
