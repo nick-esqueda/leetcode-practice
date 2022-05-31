4816386e24ce59076655ec709bfbd6390814b45e
nick-esqueda <nick_esqueda@outlook.com>
2022-05-30 20:30:31 -0500

    structy subsets no backtracking

--- 0-STRUCTY/exhaustive_recursion/subsets.py
+++ 0-STRUCTY/exhaustive_recursion/subsets.py
@@ -31,3 +31,36 @@ print(subsets(['a', 'b', 'c'])) # ->
 #   [ 'a', 'b' ],
 #   [ 'a', 'b', 'c' ]
 # ]
+
+
+def subsets(elements):
+  """
+  subsets of any list are a combination of the subsets without an element, combined with 
+    those subsets WITH the element
+  """
+  if len(elements) == 0:
+    return [[]]
+  
+  last = elements.pop()
+  subs_without_last = subsets(elements)
+  
+  subs_with_last = []
+  for sub in subs_without_last:
+    sub_copy = sub.copy()
+    sub_copy.append(last)
+    subs_with_last.append(sub_copy)
+    
+  return subs_without_last + subs_with_last
+  
+  
+print(subsets(['a', 'b', 'c'])) # ->
+# [
+#   [],
+#   [ 'c' ],
+#   [ 'b' ],
+#   [ 'b', 'c' ],
+#   [ 'a' ],
+#   [ 'a', 'c' ],
+#   [ 'a', 'b' ],
+#   [ 'a', 'b', 'c' ]
+# ]
