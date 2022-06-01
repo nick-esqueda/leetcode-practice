f709ac327f57b7cc0afefceaa7be0aabae0cf10b
nick-esqueda <nick_esqueda@outlook.com>
2022-06-01 13:57:58 -0500

    structy can color alternate solution

--- 0-STRUCTY/mixed_recall/can_color.py
+++ 0-STRUCTY/mixed_recall/can_color.py
@@ -33,3 +33,36 @@ def color_nodes(node, graph, black, white, choice):
     for nei in graph[node]:
       color_nodes(nei, graph, black, white, 'black')
     
+
+
+def can_color(graph):
+  colors = {}
+  for node in graph:
+    if node not in colors:
+      if color(node, graph, colors, True) is False:
+        return False
+  
+  return True
+  
+def color(node, graph, colors, desired_color):
+  """
+  if the node is in colors:
+    return desired_colors == colors[node]
+  paint each node the desired_color
+  for each nei:
+    color() those neis
+    if color comes back False at any point, return False
+  if you go through all neighbors and don't return F, return T
+  """
+  if node in colors:
+    return desired_color == colors[node]
+  
+  colors[node] = desired_color
+  
+  for nei in graph[node]:
+    if color(nei, graph, colors, not desired_color) is False:
+      return False
+    
+  return True
+    
+  
