f2408404c8893347cde87b9069e8532bdc5be233
nick-esqueda <nick_esqueda@outlook.com>
2022-06-04 15:40:42 -0500

    structy lexical order alternate solution

--- 0-STRUCTY/mixed_recall/lexical_order.py
+++ 0-STRUCTY/mixed_recall/lexical_order.py
@@ -13,3 +13,18 @@ def lexical_order(word_1, word_2, alphabet):
     j += 1
 
   return i == len(word_1)
+
+
+def lexical_order(word_1, word_2, alphabet):
+  max_len = max(len(word_1), len(word_2))
+  
+  for i in range(max_len):
+    value_1 = alphabet.index(word_1[i]) if i < len(word_1) else -1
+    value_2 = alphabet.index(word_2[i]) if i < len(word_2) else -1
+    
+    if value_1 < value_2:
+      return True
+    elif value_1 > value_2:
+      return False
+
+  return True
