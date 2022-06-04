f73cf9b1c93fd91807deeef079dc971326e249b7
nick-esqueda <nick_esqueda@outlook.com>
2022-06-04 17:25:29 -0500

    structy token replace alternate solution

--- 0-STRUCTY/mixed_recall/token_replace.py
+++ 0-STRUCTY/mixed_recall/token_replace.py
@@ -35,6 +35,27 @@ def token_replace(s, tokens):
 
   return res
 
+
+def token_replace(s, tokens):
+  i = 0 
+  j = 1
+  output = []
+  while i < len(s):
+    if s[i] != "$":
+      output.append(s[i])
+      i += 1
+      j += 1
+    elif s[j] != "$":
+      j += 1
+    else:
+      key = s[i: j + 1]
+      output.append(tokens[key])
+      i = j + 1
+      j = i + 1
+      
+  return ''.join(output)
+  
+
 tokens = {
   '$second$': 'beta',
   '$first$': 'alpha',
