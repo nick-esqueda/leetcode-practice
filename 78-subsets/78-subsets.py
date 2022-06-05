81e9f42a24b58dda167b452c7a434a90b2871e72
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-04 23:15:08 -0500

    Time: 62 ms (18.83%), Space: 14.2 MB (33.15%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+      """
+      a subset can be made by getting all of the subsets of the array minus 1 element and adding that element back in
+      """
+      if not nums:
+        return [[]]
+      
+      last = nums.pop()
+      subs_without_last = self.subsets(nums)
+      
+      all_subsets = [s for s in subs_without_last]
+      for sub in subs_without_last:
+        all_subsets.append([*sub, last])
+        
+      return all_subsets
