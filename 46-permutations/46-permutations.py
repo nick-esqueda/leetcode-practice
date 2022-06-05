620b9bf64ac697039f8ac166f57d24f79498ac7e
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-04 23:33:20 -0500

    Time: 67 ms (27.68%), Space: 14.1 MB (58.02%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        """
+        can get all permutations by removing one element, getting all of the perms of that subarray,
+          and then adding the element back in and shifting it through the array
+        if nums is empty, return [[]]
+        """
+        if not nums:
+          return [[]]
+        
+        last = nums.pop()
+        perms = self.permute(nums) # [[]]
+        
+        all_perms = [] # [[1]]
+        for perm in perms:
+          for i in range(len(perm) + 1):
+            all_perms.append([*perm[:i], last, *perm[i:]])
+            
+        return all_perms
+        
