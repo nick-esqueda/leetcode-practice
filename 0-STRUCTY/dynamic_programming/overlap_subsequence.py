ddda20806aae29da05ab450ea9eea98e41da33ea
nick-esqueda <nick_esqueda@outlook.com>
2022-06-20 18:06:58 -0500

    structy overlap subsequence - review

--- 0-STRUCTY/dynamic_programming/overlap_subsequence.py
+++ 0-STRUCTY/dynamic_programming/overlap_subsequence.py
@@ -29,3 +29,26 @@ def overlap_subsequence(string_1, string_2, i=0, j=0, memo={}):
     
   return memo[key]
   
+
+# 2
+def overlap_subsequence(string_1, string_2):
+  return _overlap_subsequence(string_1, string_2, 0, 0, {})
+
+def _overlap_subsequence(string_1, string_2, i, j, memo):
+  key = (i, j)
+  if key in memo:
+    return memo[key]
+  if i >= len(string_1) or j >= len(string_2):
+    return 0
+  
+  matches = 0
+  if string_1[i] == string_2[j]:
+    matches = 1 + _overlap_subsequence(string_1, string_2, i + 1, j + 1, memo)
+  else:
+    matches = max(_overlap_subsequence(string_1, string_2, i + 1, j, memo),
+                  _overlap_subsequence(string_1, string_2, i, j + 1, memo))
+  
+  memo[key] = matches
+  return memo[key]
+  
+  
