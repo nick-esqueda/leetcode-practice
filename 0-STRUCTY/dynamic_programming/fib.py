8a3a4bfa3518b3a9c248b692a1b478b79978fcdc
nick-esqueda <nick_esqueda@outlook.com>
2022-06-19 22:35:09 -0500

    structy fib - review

--- 0-STRUCTY/dynamic_programming/fib.py
+++ 0-STRUCTY/dynamic_programming/fib.py
@@ -7,3 +7,16 @@ def fib(n, memo={}):
   res = fib(n - 1, memo) + fib(n - 2, memo)
   memo[n] = res
   return res
+
+
+# 2
+def fib(n):
+  return _fib(n, {})
+
+def _fib(n, memo):
+  if n in memo:
+    return memo[n]
+  if n <= 1:
+    return n
+  memo[n] = _fib(n - 1, memo) + _fib(n - 2, memo)
+  return memo[n]
