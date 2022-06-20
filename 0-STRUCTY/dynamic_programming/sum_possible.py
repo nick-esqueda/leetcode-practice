b84ecbedcded6ddf47ca7cc4217704a6a03ec863
nick-esqueda <nick_esqueda@outlook.com>
2022-06-19 22:41:05 -0500

    structy sum possible - review

--- 0-STRUCTY/dynamic_programming/sum_possible.py
+++ 0-STRUCTY/dynamic_programming/sum_possible.py
@@ -22,3 +22,24 @@ def sum_possible(amount, numbers, memo={}):
       
   memo[amount] = False
   return False
+
+
+# 2
+def sum_possible(amount, numbers):
+  return _sum_possible(amount, numbers, {})
+
+def _sum_possible(amount, numbers, memo):
+  if amount in memo:
+    return memo[amount]
+  if amount < 0:
+    return False
+  if amount == 0:
+    return True
+  
+  for num in numbers:
+    if _sum_possible(amount - num, numbers, memo):
+      memo[amount] = True
+      return True
+    
+  memo[amount] = False
+  return False
