9480e448229b3e9f95709010ac76163c26f6a981
nick-esqueda <nick_esqueda@outlook.com>
2022-06-19 22:36:37 -0500

    structy tribonacci - review

--- 0-STRUCTY/dynamic_programming/tribonacci.py
+++ 0-STRUCTY/dynamic_programming/tribonacci.py
@@ -8,3 +8,19 @@ def tribonacci(n, memo={}):
   
   memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo)
   return memo[n]
+
+
+# 2
+def tribonacci(n):
+  return _trib(n, {})
+
+def _trib(n, memo):
+  if n in memo:
+    return memo[n]
+  if n <= 1:
+    return 0
+  if n == 2:
+    return 1
+  
+  memo[n] = _trib(n - 1, memo) + _trib(n - 2, memo) + _trib(n - 3, memo)
+  return memo[n]
