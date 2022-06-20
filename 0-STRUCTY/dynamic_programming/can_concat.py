5268aa61b43da68bc72ee9cd9469b58e68d5fe00
nick-esqueda <nick_esqueda@outlook.com>
2022-06-20 18:48:42 -0500

    structy can concat - review

--- 0-STRUCTY/dynamic_programming/can_concat.py
+++ 0-STRUCTY/dynamic_programming/can_concat.py
@@ -24,3 +24,24 @@ def _can_concat(s, words, i, memo):
   
   
 print(can_concat("oneisnone", ["on", "e", "is"]))
+
+
+
+# 2
+def can_concat(s, words):
+  return _can_concat(s, words, {})
+
+def _can_concat(s, words, memo):
+  if s in memo:
+    return memo[s]
+  if len(s) == 0:
+    return True
+  
+  for word in words:
+    if s.startswith(word):
+      if _can_concat(s[len(word):], words, memo):
+        memo[s] = True
+        return True
+
+  memo[s] = False
+  return False
