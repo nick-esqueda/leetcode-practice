322166da08ce17f0330b655232d1cf2fd66773bf
nick-esqueda <nick_esqueda@outlook.com>
2022-06-20 19:13:29 -0500

    structy paired parentheses - review

--- 0-STRUCTY/stack/paired-parentheses.py
+++ 0-STRUCTY/stack/paired-parentheses.py
@@ -41,3 +41,15 @@ def paired_parentheses(string):
       
   return count == 0
       
+      
+# 2 
+def paired_parentheses(string):
+  count = 0
+  for char in string:
+    if char == "(":
+      count += 1
+    elif char == ")":
+      count -= 1
+      if count < 0:
+        return False
+  return count == 0
