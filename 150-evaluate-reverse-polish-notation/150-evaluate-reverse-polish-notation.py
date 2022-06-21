75391e923304f448f96c01cce3cc3a965c9f7089
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-20 21:43:42 -0500

    Time: 144 ms (13.06%), Space: 14.3 MB (95.36%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.py
@@ -1,18 +1,32 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         """
-        will you need to use truncation from an import or something?
-        
         for * and / , you know that there will be 2 operands, so go back two and do that operation between those nums
         """
-        operators = "+-*/"
-        sta = []
-        for token in tokens:
-            if token in operators:
-                operand_2 = sta.pop()
-                operand_1 = sta.pop()
-                evaluated = eval(f"{operand_1}{token}{operand_2}")
-                sta.append(math.trunc(evaluated))
+        # operators = "+-*/"
+        # sta = []
+        # for token in tokens:
+        #     if token in operators:
+        #         operand_2 = sta.pop()
+        #         operand_1 = sta.pop()
+        #         evaluated = eval(f"{operand_1}{token}{operand_2}")
+        #         sta.append(math.trunc(evaluated))
+        #     else:
+        #         sta.append(int(token))
+        # return sta[0]
+        
+        stack = []
+        for c in tokens:
+            if c == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif c == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif c == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif c == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
             else:
-                sta.append(int(token))
-        return sta[0]
+                stack.append(int(c))
+        return stack[0]
