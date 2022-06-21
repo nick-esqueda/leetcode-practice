3d12891af34096f3505c45b9fe7433213648d82a
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-20 22:18:41 -0500

    Time: 31 ms (97.36%), Space: 14.1 MB (76.27%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        """
+        have a count of open and close parens
+        can choose to add an open or a close depending on some conditions:
+            if open - close == 0, can add an open but not a close
+            if open - close > 0, can add either open or close
+            can only add n number of open or close
+                (in other words, if open count > n, can't add any more opens)
+        whenever open and close counts are both == n, append that string to rtn list
+        """
+        rtn = []
+        opening = 0
+        closing = 0
+        
+        def backtrack(parens, opening, closing):
+            if opening == n and closing == n:
+                rtn.append(''.join(parens))
+                return
+            
+            if opening < n:
+                parens.append("(")
+                backtrack(parens, opening + 1, closing)
+                parens.pop()
+            if opening - closing > 0:
+                parens.append(")")
+                backtrack(parens, opening, closing + 1)
+                parens.pop()
+            
+        backtrack([], 0, 0)
+        return rtn
