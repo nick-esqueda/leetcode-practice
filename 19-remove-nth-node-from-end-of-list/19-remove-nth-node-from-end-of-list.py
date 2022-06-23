f6aaa2493b2086c23012013f16807bfa04e7c923
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-23 12:25:27 -0500

    Time: 71 ms (8.33%), Space: 14 MB (20.18%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -6,30 +6,49 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         """
-        get the len of LL by iterating once through and counting nodes
-        take len(LL) - n and that will be the 0-indexed INDEX that you need to remove
-        iterate until one before that index and then perform a deletion
+        what to do if n is 1?
         """
+        def delete_n(head, i=0):
+            if head is None:
+                return (None, 0)
+            
+            head.next, next_pos = delete_n(head.next, i + 1)
+            curr_pos = 1 + next_pos
+            
+            if curr_pos == n:
+                return (head.next, curr_pos)
+            else:
+                return (head, curr_pos)
+            
+        return delete_n(head)[0]
         
-        def get_len(head, count=0):
-            if not head:
-                return count
-            return 1 + get_len(head.next)
         
-        len_LL = get_len(head)
-        removal_idx = len_LL - n
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#         """
+#         get the len of LL by iterating once through and counting nodes
+#         take len(LL) - n and that will be the 0-indexed INDEX that you need to remove
+#         iterate until one before that index and then perform a deletion
+#         """
         
-        cur = head
-        while cur:
-            if removal_idx == 0:
-                head = head.next
-                return head
-            if removal_idx == 1:
-                old_nxt = cur.next
-                new_nxt = old_nxt.next
-                cur.next = new_nxt
-                return head
-            removal_idx -= 1
-            cur = cur.next
+#         def get_len(head, count=0):
+#             if not head:
+#                 return count
+#             return 1 + get_len(head.next)
         
-        return head
+#         len_LL = get_len(head)
+#         removal_idx = len_LL - n
+        
+#         cur = head
+#         while cur:
+#             if removal_idx == 0:
+#                 head = head.next
+#                 return head
+#             if removal_idx == 1:
+#                 old_nxt = cur.next
+#                 new_nxt = old_nxt.next
+#                 cur.next = new_nxt
+#                 return head
+#             removal_idx -= 1
+#             cur = cur.next
+        
+#         return head
