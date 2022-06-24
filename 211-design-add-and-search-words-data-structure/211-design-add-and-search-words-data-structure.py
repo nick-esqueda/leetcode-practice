1f0263d646bcf872ab505151bdf3b189f5a6c2ba
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-24 16:53:36 -0500

    Time: 18955 ms (6.92%), Space: 78 MB (53.10%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self):
+        # self.char = char
+        self.children = {}
+        self.is_word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node()
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = Node()
+            node = node.children[char]
+        node.is_word = True
+
+    def search(self, word: str) -> bool:
+        """
+        if the char is a dot, need to go search through all children
+        can maybe do this recursively, throwing in the rest of the word
+        """
+        def backtrack(node, i):
+            if i >= len(word) and node.is_word:
+                return True
+            
+            while i < len(word) and word[i] in node.children:
+                node = node.children[word[i]]
+                i += 1
+                
+            if i == len(word):
+                return node.is_word
+            
+            elif word[i] == ".":
+                for child in node.children:
+                    if backtrack(node.children[child], i + 1):
+                        return True
+                    
+            return False
+                
+        return backtrack(self.root, 0)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
