992e6f1891f8755eb9ef7b8fb78da4ad14ca2ee9
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-24 21:33:53 -0500

    Time: 1464 ms (70.43%), Space: 16.5 MB (17.89%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.py
+++ 212-word-search-ii/212-word-search-ii.py
@@ -60,64 +60,61 @@ class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         """
         if you create a prefix tree out of 'words', then you only have to scan through the board once
-        scan through and look for one of the root's chldren, and once you're on one, start of a backtracking DFS
+        scan through and look for one of the root's chldren, and once you're on one, start backtracking DFS
         during the DFS, you'll have to scan through both the board and the trie at the same time
-        if you find the next child, move pointer down the tree, and recurse with the next node
-        if the word is found, add to a result list
-        after iterating through the whole board, return the result list, even if there's stuff in the trie
+        recurse with the current node, and if the next position is in node.children, advance node, and keep going
+        if the word is found, add to a result list, and delete word from trie so you don't go back down the same path
+        after iterating through the whole board, return the result list, even if there's still stuff in the trie
         
-        how are you able to pop back up in the DFS and have the trie pointer move back?
-        how will you delete the word from the trie after you find it?
-            might not be the best way, but you can just change the is_word variable to mark it as done
-        how will you know what word to push to found?
-            maybe build a string as you recurse downwards?
+        BACKTRACKING:
+        if the board[r][c] not in node.children, return 
+        need to also pass in the node itself as you recurse down
+        the grid will lead
+            recurse in all 4 directions first, and then see if that letter is in the node's children
+            if so, keep going, but if not, you then know that you shouldn't go that direction
+            it's as if the recursion is eager, but asks for permission before going too far
+        should probably just be building the string as an array (for efficiency) as you go down
+        once you find the end of the word, just append to found and delete the word from the trie
         """
         trie = Trie()
         for word in words:
             trie.insert(word)
         
+        def find_word(r, c, node, word, vis):
+            if (r < 0 or c < 0 or r >= len(board) or c >= len(board[0])):
+                return
+            board_char = board[r][c]
+            if (r, c) in vis:
+                return
+            if board_char not in node.children:
+                return
+
+            vis.add((r, c))
+            word.append(board_char)
+            node = node.children[board_char]
+            if node.is_word:
+                full_word = ''.join(word)
+                trie.delete(full_word)
+                found.add(full_word)
+
+            find_word(r - 1, c, node, word, vis)
+            find_word(r + 1, c, node, word, vis)
+            find_word(r, c - 1, node, word, vis)
+            find_word(r, c + 1, node, word, vis)
+
+            vis.remove((r, c))
+            word.pop()
+            return
+
         found = set()
         for r in range(len(board)):
             for c in range(len(board[r])):
                 board_char = board[r][c]
                 if board_char in trie.root.children:
-                    self.find_word(board, r, c, trie.root, [], found, set(), trie)
+                    find_word(r, c, trie.root, [], set())
                    
         return found
                     
-    def find_word(self, board, r, c, node, word, found, vis, trie):
-        """
-        if the board[r][c] != the node char, return false
-        need to also pass in the node itself as you recurse down
-        what should you be returning?
-        should probably just be building the string as an array (for efficiency) as you go down
-        once you find the end of the word, just append to found
-        """
-        if (r < 0 or c < 0 or r >= len(board) or c >= len(board[0])):
-            return
-        board_char = board[r][c]
-        if (r, c) in vis:
-            return
-        if board_char not in node.children:
-            return
-        
-        vis.add((r, c))
-        word.append(board_char)
-        node = node.children[board_char]
-        if node.is_word:
-            full_word = ''.join(word)
-            trie.delete(full_word)
-            found.add(full_word)
-        
-        self.find_word(board, r - 1, c, node, word, found, vis, trie)
-        self.find_word(board, r + 1, c, node, word, found, vis, trie)
-        self.find_word(board, r, c - 1, node, word, found, vis, trie)
-        self.find_word(board, r, c + 1, node, word, found, vis, trie)
-
-        vis.remove((r, c))
-        word.pop()
-        return
-    
     
     
     
