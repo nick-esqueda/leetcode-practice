3abda86efc29524b44ff120f995d3d5d51cb8cba
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-25 19:24:08 -0500

    Time: 38 ms (78.59%), Space: 13.9 MB (61.38%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        """
+        stones must be sorted so you don't have to rescan every time to get the 2 largest
+        can use a max heap so that we always have access to the heaviest stone
+        heapppop twice and subtract the smaller of the two from the larger
+        if that difference is 0, do nothing
+        otherwise, put the remainder back into the max heap
+        keep doing all of this until the length of the max heap is <= 1
+        return that last stone's weight, but if there is not last stone, return 0
+        """
+        stones = list(map(lambda x: x*-1, stones))
+        heapq.heapify(stones)
+        
+        while len(stones) > 1:
+            s1 = heapq.heappop(stones)
+            s2 = heapq.heappop(stones)
+            lighter, heavier = min(s1, s2), max(s1, s2)
+            crushed = heavier - lighter
+            if crushed > 0:
+                heapq.heappush(stones, crushed*-1)
+        
+        return stones[0]*-1 if stones else 0
