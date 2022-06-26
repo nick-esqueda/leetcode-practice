d8745b30a558a6eabed7c50b62fd6fc98583a70f
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-26 12:46:11 -0500

    Time: 52 ms (34.53%), Space: 14.1 MB (17.92%) - LeetHub

---
+++ 355-design-twitter/355-design-twitter.py
@@ -0,0 +1,51 @@
+class Twitter:
+
+    def __init__(self):
+        """
+        we want the tweets sorted in order of posted
+        that way, we can just iterate through until we have 10 posts that have a userId that is in the user's following set
+        """
+        self.follows = defaultdict(set) # follower: set(followee, followee)
+        self.tweets = [] 
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        """
+        if user doesn't exist, add it to follows
+        will tweetId's come in always increasing order?
+        if the userId is not in follows, add it
+        add the tweet to tweets set
+        """
+        if userId not in self.follows:
+            self.follows[userId].add(userId)
+        self.tweets.append((userId, tweetId))
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        """
+        iterate through tweets until you have a collection of 10 posts that have a userId in the user's following set
+        """
+        following = self.follows[userId]
+        tweets = []
+        i = len(self.tweets) - 1
+        while len(tweets) < 10 and i >= 0:
+            user, tweet = self.tweets[i]
+            if user in following:
+                tweets.append(tweet)
+            i -= 1
+        return tweets
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.follows:
+            self.follows[followerId].add(followerId)
+        self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.follows[followerId]:
+            self.follows[followerId].remove(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
