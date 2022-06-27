74e862ec22fd95bbc8847f62b71365350d9d11cb
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-27 13:27:46 -0500

    Time: 152 ms (27.11%), Space: 14.8 MB (31.98%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        """
+        as you traverse, if you deleted the edge that you just came along, then you won't get cycles
+        choose the airport with a lower lexical value
+        
+        can't really for loop through the adjlist array, since the size of it will change every iteration...
+        but, you could just iterate until that array is empty and pop off every time and visit that nei
+        """
+        graph = self.build_graph(tickets)
+        itin = ["JFK"]
+        def dft(node):
+            if len(itin) == len(tickets) + 1:
+                return True
+            if node not in graph:
+                return False
+            
+            copy = list(graph[node])
+            for nei in copy:
+                graph[node].pop(0)
+                itin.append(nei)
+                if dft(nei):
+                    return True
+                graph[node].append(nei)
+                itin.pop()
+            return False
+        
+        dft("JFK")
+        return itin
+        
+    def build_graph(self, edges):
+        graph = defaultdict(list)
+        edges.sort()
+        for a, b in edges:
+            graph[a].append(b)
+        return graph
