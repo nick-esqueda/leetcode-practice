71f936e8fee32241b3b73629f0f362b48f79f770
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-28 16:52:14 -0500

    Time: 8765 ms (5.01%), Space: 123 MB (39.28%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        """
+        you have an array of point coords [[x, y], [x, y]]
+        
+        set up a heap initialized with the first point in the graph
+        pop from the heap to get the current node 
+        print node
+        if this node is not already in visited:
+            increment the total cost var
+            calculate the weight to all of the other nodes and store inside tuple with node
+            add all of those neighbors/weights to the heap
+        """
+        adj = { i: [] for i in range(len(points)) }
+        for i in range(len(points)):
+            x1, y1 = points[i]
+            for j in range(i + 1, len(points)):
+                x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append((dist, j))
+                adj[j].append((dist, i))
+                
+        min_cost = 0
+        heap = [(0, 0)]
+        heapq.heapify(heap)
+        vis = set()
+        while heap:
+            weight, coord_idx = heapq.heappop(heap)
+            
+            if coord_idx not in vis:
+                min_cost += weight
+                vis.add(coord_idx)
+                for nei in adj[coord_idx]:
+                    heapq.heappush(heap, nei)
+                    
+        return min_cost
+        
