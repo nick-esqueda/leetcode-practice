656fdee262acb03a64f865dfea8dfa6e72182a0d
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-06-28 23:42:53 -0500

    Time: 154 ms (50.59%), Space: 15.6 MB (69.95%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,41 +1,33 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-      """
-      topological order
-      need to account for cycles
-      need to handle courses that don't have prerequisites
-      """
-      graph = self.create_graph(prerequisites, numCourses)
-        
-      num_parents = {}
-      for node in graph:
-        num_parents[node] = 0
-      for node in graph:
-        for child in graph[node]:
-          num_parents[child] += 1
-          
-      order = []
-      ready = [node for node in num_parents if num_parents[node] == 0]
-      while ready:
-        node = ready.pop()
-        order.append(node)
-        for child in graph[node]:
-          num_parents[child] -= 1
-          if num_parents[child] == 0:
-            ready.append(child)
-            
-      for node in num_parents:
-        if num_parents[node] > 0:
-          return []
-        
-      return order[::-1]
-        
-    def create_graph(self, edges, numCourses):
-      graph = { i: [] for i in range(numCourses) }
-      for course, prereq in edges:
-        if course not in graph:
-          graph[course] = []
-        if prereq not in graph:
-          graph[prereq] = []
-        graph[course].append(prereq)
-      return graph
+        """
+        0 4 if there are numCourses = 5
+        if you get the topological order (from "starting" nodes to "ending nodes"), you can return the reverse of that order
+        if you have to take a course to take another course, but that couse requires the first as a prereq, that's a cycle
+        need to be able to detect cycles - return [] if cycle is detected
+        you'll notice there is a cycle if there are nodes in the parent count list you make that are > 0
+        this is because you would have exited early because the stack was empty, since something had 2 parents, but you couldn't get to the 2nd
+        """
+        adj = { i: [] for i in range(numCourses) }
+        for a, b in prerequisites:
+            adj[a].append(b)
+
+        parent_counts = { i: 0 for i in adj }
+        for parent in adj:
+            for child in adj[parent]:
+                parent_counts[child] += 1
+     
+        sta = [course for course in parent_counts if parent_counts[course] == 0]
+        top_order = []
+        while sta:
+            course = sta.pop()
+            top_order.append(course)
+            for prereq in adj[course]:
+                parent_counts[prereq] -= 1
+                if parent_counts[prereq] == 0:
+                    sta.append(prereq)
+      
+        for course in parent_counts:
+            if parent_counts[course] > 0:
+                return []
+        return top_order[::-1]
