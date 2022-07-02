58cbcf640a16cc8afbc2afbd666be8427af266e9
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-02 17:42:20 -0500

    Time: 61 ms (48.04%), Space: 14 MB (45.06%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,6 +1,29 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         """
+        whenever i is right after the end of the string, output is True
+        decrement backwards through s
+        check s starting at i if that substr .startswith the word
+        if it does, check the tab at i + len(str), and if it's True, set the tab at this i to True, else False
+        keep iterating backwards
+        after iterating return the value you have for tab[0]
+        """
+        tab = [False] * (len(s) + 1)
+        tab[-1] = True
+        
+        for i in range(len(s)-1, -1, -1):
+            substr = s[i:]
+            for word in wordDict:
+                if substr.startswith(word) and tab[i + len(word)] is True:
+                    tab[i] = True
+                    break
+        return tab[0]
+        
+        
+    
+    
+    def wordBreak_TOPDOWN(self, s: str, wordDict: List[str]) -> bool:
+        """
         can choose only words that .startswith the front of the word
         can use an i pointer to indicate where to start the rest of s
         once you find a match, move i up by the length of the word from wordDict
