b843f8675208e438feeea8765fdde5a4c09e27cf
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-03 14:08:28 -0500

    Time: 455 ms (50.13%), Space: 14.3 MB (50.57%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,6 +1,42 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         """
+        2D tab with coins as rows and amounts (amount all the way down to 0) as columns
+        at each pos, set the pos in tab to the sum of two things:
+        1. the value of the tab after you subtract the coin from the amount (amount - coin)
+            - you'll look in the same row - tab[this coin row][amount - coin]
+        2. the number if the row below in the same column
+            - this is the number of combinations using the rest of the coins
+        you'll start with a default row with all 0's
+            - b/c there are 0 ways to get any amount with no coins to choose from
+            
+        ---
+        you will have 0,1,2 for the cols, but obviously those are indices. need to key into the coins array with those indices
+        ---
+           0  1  2  3  4  5      
+        1 [1, 1, 2, 2, 3, 4]
+        2 [1, 0, 1, 0, 1, 1]
+        5 [1, 0, 0, 0, 0, 1]
+        """
+        prev_coin_row = [0] * (amount + 1)
+        
+        for i in range(len(coins) - 1, -1, -1):
+            coin = coins[i]
+            row = [0] * (amount + 1) 
+            row[0] = 1
+            for amt in range(1, len(row)):
+                combos = 0
+                if amt - coin >= 0:
+                    combos += row[amt - coin]
+                combos += prev_coin_row[amt]
+                row[amt] = combos
+            prev_coin_row = row
+            
+        return prev_coin_row[-1]
+    
+    
+    def change_TOPDOWN(self, amount: int, coins: List[int]) -> int:
+        """
         making the decision of what coin to choose will end up with multiple permutations of the same answer
             - b/c you are able to revisit a coin you've already used in the past
         what you can instead do is choose the quantity of each coin that you want to use
