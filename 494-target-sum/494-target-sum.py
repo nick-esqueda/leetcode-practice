89969477d959bf273f681175e02e131d76d14857
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-03 16:11:58 -0500

    Time: 450 ms (52.38%), Space: 36 MB (14.70%) - LeetHub

---
+++ 494-target-sum/494-target-sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        """
+        mult by -1 to get the inverse
+        can choose to multiply by -1 for this num or leave it
+        after you get to the end of nums, sum all of those up and check against target
+        if the sum == target, return 1 since you found a way to hit target
+        return the sum of the different decisions
+        """
+        memo = {}
+        def get_combos(i, run_sum):
+            key = (i, run_sum)
+            if key in memo:
+                return memo[key]
+            if i >= len(nums):
+                return 1 if run_sum == target else 0 
+            
+            combos = 0
+            
+            run_sum += nums[i]
+            combos += get_combos(i + 1, run_sum)
+            
+            run_sum -= nums[i]
+            run_sum -= nums[i]
+            combos += get_combos(i + 1, run_sum)
+            
+            memo[key] = combos
+            return combos
+        
+        return get_combos(0, 0)
