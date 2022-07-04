ac740a21a68b5ac3181f11f0510a6fefe956510a
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-03 20:05:35 -0500

    Time: 52 ms (76.44%), Space: 14 MB (73.10%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,6 +1,49 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         """
+        if you add up the indices of s1 and s2, you'll get the index in s3 that holds the remainder of the chars
+        if you have a 2D tab with s1 and s2 chars as rows/cols, each pos will be a boolean whether or not you
+            can interleave the remaining s1 and s2 chars together to make up the remainder of s3
+        the bottom right diagonal will be True, since that's the case where you used all chars of both strings
+        the bottommost and rightmost row and column represent the ability to create the rest of s3 with just one string
+            - need to determine these manually / these are not base cases
+        starting at the bottommost row, work backwards and calculate the index of s3 you need to look at
+        at each position, calculate the s3 idx by adding both r and c together
+        that s3 idx will be your char to match
+        if none of the s1 or s2 chars match, set that pos to False
+        if one of the chars match, look at the corresponding down or right pos, and set pos equal to whatever is in that pos
+        if both of the chars match, look at both right and down pos's and if either is True, set pos to True
+        """
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        s1 += '!'
+        s2 += '!'
+        tab = [[False] * (len(s1)) for _ in range(len(s2))]
+        
+        for r in range(len(s2) - 1, -1, -1):
+            for c in range(len(s1) - 1, -1, -1):
+                if r + c >= len(s3):
+                    tab[r][c] = True
+                    continue
+                target = s3[r + c]
+                if s2[r] == target and s1[c] == target:
+                    tab[r][c] = tab[r + 1][c] or tab[r][c + 1] # either bottom or right
+                elif s2[r] == target:
+                    tab[r][c] = tab[r + 1][c] # look below
+                elif s1[c] == target:
+                    tab[r][c] = tab[r][c + 1] # look right
+                else:
+                    tab[r][c] = False # no matches - F
+                    
+        return tab[0][0]
+                
+            
+        
+        
+        
+    def isInterleave_TOPDOWN(self, s1: str, s2: str, s3: str) -> bool:
+        """
         if start of both string are ==, must choose between either one to use
         if only one string starts with the first char in s3, have to use that one
         if neither string starts with the first char in s3, return False (s3 cannot be formed)
