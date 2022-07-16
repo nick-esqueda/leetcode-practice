865ad3853d6490c74eebcb750f7b2a1b694697dc
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-15 22:58:21 -0500

    Time: 39 ms (97.32%), Space: 14.4 MB (18.93%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,38 +1,44 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        """
-        if the midpoint is greater than the left, the left side is still normal
-        if the midpoint is less than the left, the left side has larger numbers there and starts over at some point
-        if the midpoint is greater than the right, all of the numbers to the right are less than target
-        if the midpoint is less than the right, the right side is still normal
-        if midpoint is between two number's values, everything is like normal
+        """       
+        if mid is greater than right, and the target is less, move left up (search right half)
+        if mid is less than right, and the target is
+        if mid is between left and right, everything is normal, so move accordingly
         
-        if the target is between mid and right, move lo up (mid has to be lower than right)
-        if the target is between mid and left, move hi down (mid has to be higher than right)
         
-        if mid is higher than right and the target is less than right, move lo up, else move hi down
-        if mid is lower than left and target is higher than left, move hi down, else move lo up
+        [3, 1] t = 1
+        lm  r
         
-        if the nums weren't unique, you'd run into a case where right/left == mid, and then you couldn't determine direction
         """
         
-        lo, hi = 0, len(nums) - 1
-        while lo <= hi:
-            mid = (lo + hi) // 2
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
             
             if nums[mid] == target:
                 return mid
             
-            if nums[lo] <= target < nums[mid]:
-                hi = mid - 1
-            elif nums[mid] < target <= nums[hi]:
-                lo = mid + 1
-            elif nums[mid] < nums[lo] and target >= nums[lo]:
-                hi = mid - 1
-            elif nums[mid] > nums[hi] and target <= nums[hi]:
-                lo = mid + 1
-            # to take care of numbers that don't exist, or for overlapping errors
-            else:
-                lo += 1
+            # everything is normal
+            if nums[l] <= nums[mid] <= nums[r]:
+                if target < nums[mid]:
+                    r = mid - 1
+                elif target > nums[mid]:
+                    l = mid + 1
+            
+            # the split is to the left
+            elif nums[l] > nums[mid]:
+                if target < nums[mid] or target >= nums[l]:
+                    r = mid - 1
+                elif target > nums[mid]: # this could be just else?
+                    l = mid + 1
+                    
+            # the split is to the right
+            elif nums[r] < nums[mid]:
+                if target > nums[mid] or target <= nums[r]:
+                    l = mid + 1
+                elif target < nums[mid]: # this could be just else?
+                    r = mid - 1
                 
         return -1
+            
+        
