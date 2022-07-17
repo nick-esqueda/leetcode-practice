be0a88f76912577d577137e89c2f34707bcf5671
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-17 16:37:38 -0500

    Time: 57 ms (26.22%), Space: 13.8 MB (94.81%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -3,10 +3,11 @@ class Solution:
         """
         to find if there is a 1 in a certain position, you can & the integer with whatever byte has only a 1 in the place you are looking for
         you COULD & n with a new number representing each position each time, but then you'd have to calculate the power of 2 for each position
+        a more efficient way to do it would be to right shift once for every position to see if it &'s with 1 every time
         """
         count = 0
         for i in range(32):
-            mask = 2 ** i
-            if (n & mask) != 0:
+            if n & 1:
                 count += 1
-        return count                
+            n = n >> 1
+        return count
