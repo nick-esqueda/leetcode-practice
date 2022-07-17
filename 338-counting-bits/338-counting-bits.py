bf143dc562eafffe0ee2a8caa330f3f19a10e468
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-17 17:57:04 -0500

    Time: 123 ms (66.40%), Space: 20.9 MB (29.70%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -4,16 +4,24 @@ class Solution:
         from 0 up to n inclusive (i), calculate how many 1's are in the binary representation of i and store that in the array at that same index
         
         can have a helper method to find how many 1's are in each int, and call it on every iteration up to n
+        this will become progressively more inefficient with higher values of n since you're doing progressively more work each iteration
+        
+        if a num is a power of 2, then it will only have one 1
+        once you move up a power of 2, the rest of the places will be what were just calculated from 0 up until the current power of 2
+        add one to all of the previous calculations to get the number of ones for the curr num
+        
+        offset variable
+        whenever you get to a new power 2, reassign offset variable
+        you know you're at a new power of 2 if offset * 2 == curr num, because offset will always be a power of two
+        once you get to a new power of 2, push 1 to the rtn arr, then set the offset var to be the curr power of 2
         """
-        rtn = []
-        for i in range(n + 1):
-            rtn.append(self.num_ones(i))
+        rtn = [0]
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset *= 2
+                rtn.append(1)
+            else:
+                count = 1 + rtn[i - offset]
+                rtn.append(count)
         return rtn
-    
-    def num_ones(self, n):
-        count = 0
-        while n != 0:
-            if n & 1:
-                count += 1
-            n = n >> 1
-        return count
