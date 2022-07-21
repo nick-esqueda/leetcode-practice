44df3aa9405f72235fd53403bb7cb1d93dfe09f7
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-20 21:13:59 -0500

    Time: 145 ms (87.23%), Space: 15 MB (58.78%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,5 +1,24 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
+        """
+        can move two pointers around to represent 'levels'
+        each level will contain all of the vals you can use to jump
+        set the hi pointer to the farthest idx you could jump to
+        set lo to hi+1
+        increment count because you just moved on another level
+        loop through everything again until your right pointer is out of bounds
+        """
+        jumps = 0
+        l = r = 0
+        while r < len(nums) - 1:
+            max_jump = 0
+            for i in range(l, r + 1): # loop through level to get the farthest index
+                max_jump = max(max_jump, i + nums[i])
+            l, r = r + 1, max_jump
+            jumps += 1
+        return jumps
+    
+    def jump_TOPDOWN(self, nums: List[int]) -> int:
         memo = {}
         def get_shortest(i):
             if i in memo:
@@ -18,4 +37,4 @@ class Solution:
             return min_jumps
         
         return get_shortest(0)
-            
+        
