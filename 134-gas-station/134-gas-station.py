2dad7ac1d8c5209b64d2628a0b66632e86af50ce
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-22 12:09:10 -0500

    Time: 980 ms (49.93%), Space: 19.3 MB (26.04%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,27 +1,7 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         """
-        gas[prices of gas] -> [1, 2, 3, 4, 5] <- ! will be returning one of these indices
-        cost[amount of gas that'll burn to go to the next station] -> [3, 4, 5, 1, 2]
-        
-        tank = 0 + 4 = 4 - 1 + 5 = 8 - 2 + 1 = 7 - 3 + 2 = 6 - 4 + 3 = 5 - 5 + 4 = 4
-        g[1, 2, 3, 4, 5]
-                   i                                  
-        c[3, 4, 5, 1, 2]
-        
-         [(1,-1),  ,  ,  ,  ,]
-        
-        tank = 0 + 4 = (4 - 3) + 2 = (3 - 3) + 3 = (3 - 4) x
-        g[2, 3, 4]
-                i        
-        c[3, 4, 3]
-        
-        
-        if the tank ever goes negative, you can't start at that position
-        
-        start the traversal at every index
-        if the traversal comes back as -1, save that to a memo/tab
-        
+        idk
         """
         if sum(gas) < sum(cost):
             return -1
