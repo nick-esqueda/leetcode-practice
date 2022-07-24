e67a18ca4cae2fe1df535572acd0b8ab69b4a732
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-24 11:34:42 -0500

    Time: 125 ms (75.57%), Space: 17.7 MB (6.88%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,6 +1,40 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         """
+        return a topological sorting of the courses
+        if there is a cycle, you can't finish all courses, so return []
+        during each dfs, keep track of path and done sets - if you see a node in path, there is a cycle
+        """
+        adj = { i: [] for i in range(numCourses) }
+        for a, b in prerequisites:
+            adj[a].append(b)
+            
+        def topsort(node):
+            if node in done:
+                return True
+            if node in path:
+                return False
+            
+            path.add(node)
+            for nei in adj[node]:
+                if topsort(nei) is False:
+                    return False
+            stack.append(node)
+            path.remove(node)
+            done.add(node)
+            
+        stack = []
+        done = set()
+        for course in adj:
+            path = set()
+            if topsort(course) is False:
+                return []
+        return stack
+        
+    
+    
+    def findOrder_TOPOLOGICAL_SORT_ITERATIVE(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        """
         0 4 if there are numCourses = 5
         if you get the topological order (from "starting" nodes to "ending nodes"), you can return the reverse of that order
         if you have to take a course to take another course, but that couse requires the first as a prereq, that's a cycle
