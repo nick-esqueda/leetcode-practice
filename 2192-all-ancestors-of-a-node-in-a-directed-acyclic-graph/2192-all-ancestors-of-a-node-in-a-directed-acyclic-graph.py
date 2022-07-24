ad241371c57830b802231f30c090e7af59366181
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-24 17:12:12 -0500

    Time: 1590 ms (32.45%), Space: 32.7 MB (60.19%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,6 +1,29 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         """
+        if you perform a dfs on every single node (including visited nodes), you can add the starting node of the dfs to the ancestor list of each child node
+        """
+        adj = { i: [] for i in range(n) }
+        for a, b in edges:
+            adj[a].append(b)
+            
+        def get_ancestors(node, start):
+            if not (ancestors[node] and ancestors[node][-1] == start):
+                if node != start:
+                    ancestors[node].append(start)
+                
+                for child in adj[node]:
+                    get_ancestors(child, start)
+            
+        ancestors = [[] for _ in range(n)]    
+        for node in range(n):
+            get_ancestors(node, node)
+        
+        return ancestors
+
+
+    def getAncestors_KAHNS(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        """
         add the current node into the ancestor list for each child
         make sure that you process the least value parent first
         """
