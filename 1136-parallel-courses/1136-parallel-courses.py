395b4ed05d193864cdc77cdb808020d25b6439d3
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-24 13:41:23 -0500

    Time: 540 ms (14.35%), Space: 17.4 MB (35.30%) - LeetHub

---
+++ 1136-parallel-courses/1136-parallel-courses.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        """
+        start at the nodes with 0 parents, and with each level, increment a semester count
+        modified kahn's algorithm
+        if the number of nodes you've seen after traversing is less than the amount of nodes in the graph, you had a cycle
+        this is because you will only push nodes on to the queue if they have 0 parents left. a cycle node will always have another parent that you can't get to
+        """
+        
+        adj = { i: [] for i in range(1, n + 1) }
+        in_degree = [0] * (n + 1)
+        for a, b in relations:
+            adj[a].append(b)
+            in_degree[b] += 1
+            
+        q = []
+        for course in adj:
+            if in_degree[course] == 0:
+                q.append(course)
+               
+        sems = 0
+        vis = set()
+        while q:
+            sems += 1
+            new_q = []
+            for course in q:
+                vis.add(course)
+                for child in adj[course]:
+                    in_degree[child] -= 1
+                    if in_degree[child] == 0:
+                        new_q.append(child)
+            q = new_q
+            
+        return sems if len(vis) == n else -1
+            
+        
+        
