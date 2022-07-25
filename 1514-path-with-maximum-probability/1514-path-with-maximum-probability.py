fcdebc9a72ae3e2c207a5be0242d495b3f482ed5
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-25 18:50:34 -0500

    Time: 838 ms (80.04%), Space: 28.4 MB (5.20%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        """
+        dijkstra SSSP
+        """
+        adj = { i: [] for i in range(n) }
+        for i, edge in enumerate(edges):
+            u, v = edge
+            adj[u].append((succProb[i], v))
+            adj[v].append((succProb[i], u))
+        
+        print(adj)
+        
+        vis = set()
+        pq = [(-1.0, start)] # want the max path - max heap instead of min heap
+        while pq:
+            wgt, node = heapq.heappop(pq)
+            if node in vis:
+                continue
+                
+            vis.add(node)
+            if node == end:
+                return -wgt
+            
+            for nei_wgt, nei in adj[node]:
+                if nei not in vis:
+                    heapq.heappush(pq, (-abs(nei_wgt * wgt), nei))
+        return 0
