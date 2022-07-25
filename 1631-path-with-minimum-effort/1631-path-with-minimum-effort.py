c6dd65e799046d84b0383cea36a49fb51ad8ab9d
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-25 18:08:19 -0500

    Time: 1519 ms (41.04%), Space: 16.5 MB (39.04%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -6,15 +6,14 @@ class Solution:
         since you will always choose to expand the node with the best cost, you can just keep track of a max of all of those edge costs
             but ONLY if it is in the final path
             
-        
         dijkstra will give you the length of the shortest path, and maybe also the path
         but you don't want the shortest path, you want the path that has the smallest effort
         if dijkstra gets to a node that has a higher weight, it will only go back to that edge if all other edges are also that same weight or greater
             this is because the heap will always have the edge with minimum weight (effort) at the top of it
         knowing this, this means that you can always check for a max of the minimum effort while working towards the bottom right cell
         but you have to make sure that you are on the final path before you take those maxes into account...
+        if you make sure to return once you get to the bottom right, then you know for certain that the only edges that were taken into the max calculation were the smallest ones
         """
-        
         most_effort = float('-inf')
         vis = set()
         pq = [(0, 0, 0)]
@@ -29,20 +28,16 @@ class Solution:
                 return most_effort
             
             neis = self.get_neighbors(heights, r, c)
-            for wgt, nr, nc in neis:
+            for nr, nc in neis:
                 if (nr, nc) not in vis:
-                    heapq.heappush(pq, (wgt, nr, nc))
+                    nei_effort = abs(heights[nr][nc] - heights[r][c])
+                    heapq.heappush(pq, (nei_effort, nr, nc))
         
     
     def get_neighbors(self, matrix, r, c):
-        deltas = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        
-        neis = []
-        for dr, dc in deltas:
-            nr, nc = r + dr, c + dc
-            if nr in range(len(matrix)) and nc in range(len(matrix[0])):
-                wgt = abs(matrix[nr][nc] - matrix[r][c])
-                neis.append((wgt, nr, nc))
-        return neis
+        neis = [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]
+        def is_valid(coord):
+            return coord[0] in range(len(matrix)) and coord[1] in range(len(matrix[0]))
+        return [nei for nei in neis if is_valid(nei)]
             
             
