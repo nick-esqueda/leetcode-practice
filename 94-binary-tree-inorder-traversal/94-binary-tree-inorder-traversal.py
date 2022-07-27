da42cae300269c359890ea9ebf9f3b2d4dc6cfdc
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-07-26 21:13:19 -0500

    Time: 34 ms (87.30%), Space: 13.8 MB (96.73%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -6,6 +6,24 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        once you're all the way down the left (no more lefts), you want to push to inorder, then pop back up to par
+        """
+        inorder = []
+        sta = []
+        cur = root
+        while sta or cur:
+            if cur:
+                sta.append(cur)
+                cur = cur.left
+            else:
+                cur = sta.pop()
+                inorder.append(cur.val)
+                cur = cur.right
+        return inorder
+            
+        
+    def inorderTraversal_RECURSIVE(self, root: Optional[TreeNode]) -> List[int]:
         inorder = []
         def dfs(root):
             if root is None:
