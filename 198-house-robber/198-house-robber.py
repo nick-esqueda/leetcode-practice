f1536fe3669a95e8007b29d5cf2367c05fdbed6a
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-08-11 21:18:11 -0500

    Time: 33 ms (92.48%), Space: 13.9 MB (66.18%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,16 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         """
-        can choose to either rob this one and skip the next
-        or can choose to skip this one and rob the next one
-        
-        the max you can rob of the first house is just that value, and the same for the second 
+        [21, 21, 1, 1, 22]
+         i
+         
+        each tab index will be the max of either -
+            > the position right after the curr index
+            > the sum of the num at this index + the num at i + 2
         """
         if len(nums) <= 2:
             return max(nums)
         
-        nums[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            nums[i] = max(nums[i] + nums[i - 2], nums[i - 1])
-
-        return nums[-1]
+        nums[-2] = max(nums[-2], nums[-1])        
+        for i in range(len(nums) - 3, -1, -1):
+            nums[i] = max(nums[i + 1],
+                          nums[i] + nums[i + 2])
+            
+        return nums[0]
+        
