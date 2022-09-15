96a4930a54ef9a6d5ec33998f159a6be8f4d8ea7
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-09-15 15:18:59 -0500

    Time: 203 ms (93.39%), Space: 15.4 MB (44.83%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.py
@@ -4,7 +4,7 @@ class Solution:
         the sum of the two smaller sides must be greater than the large side
         """
         nums.sort(reverse=True)
-        for i in range(len(nums) - 2): #might be -3
+        for i in range(len(nums) - 2):
             if nums[i + 1] + nums[i + 2] > nums[i]:
                 return nums[i] + nums[i + 1] + nums[i + 2]
         return 0
