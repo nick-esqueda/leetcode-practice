3651adaa84ebeee4e542a5cb2cf269862276aed3
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-09-15 23:17:11 -0500

    Time: 140 ms, Memory: 14.1 MB - LeetHub

--- min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,6 +1,25 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         """
+        loop backwards to start at the base cases
+        recurrence relation:
+            the min cost to the top from here is this cost + the cheaper of the min costs for the next two steps
+            (work backwards)
+            dp[i] = costs[i] + min(dp[i + 1], dp[i + 2])
+        """
+        if len(cost) == 1:
+            return cost[0]
+        
+        tab = [float('inf')] * len(cost)
+        tab[-1], tab[-2] = cost[-1], cost[-2]
+        
+        for i in range(len(cost) - 3, -1, -1):
+            tab[i] = cost[i] + min(tab[i + 1], tab[i + 2])
+        return min(tab[0], tab[1])
+        
+    
+    def minCostClimbingStairs_TOPDOWN(self, cost: List[int]) -> int:
+        """
         PROBLEM:
             minumum cost to reach 1 step out of bounds
             once you pay the cost at i, you can move forward either 1 or 2 steps
