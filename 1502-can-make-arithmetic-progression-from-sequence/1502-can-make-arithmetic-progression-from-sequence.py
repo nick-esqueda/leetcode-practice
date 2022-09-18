4d4383910b910190b4d031dff275d7799127e5e1
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-09-18 02:08:10 -0500

    Time: 88 ms (13.17%), Space: 13.9 MB (66.45%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -2,9 +2,6 @@ class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
         """
         can you rearrange the array so that the difference between each pair of consecutive numbers is the same?
-        
-        all of the numbers must be the same difference apart from one number
-        
         """
         arr.sort()
         
