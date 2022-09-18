4e9e905a40c971e0a013ffab39439d679c07896a
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-09-18 17:43:11 -0500

    Time: 1038 ms (31.12%), Space: 21.9 MB (79.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,6 +1,25 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         """
+        start at the base cases.
+        dp(i, j) = 0                                <--- if i == len(text1) || j == len(text2)        
+        dp(i, j) = 1 + dp(i + 1, j + 1)             <--- if text1[i] == text2[j]
+        dp(i, j) = max(dp(i + 1, j), dp(i, j + 1))  <--- if text1[i] != text2[j]
+        """
+        l1, l2 = len(text1), len(text2)
+        tab = [[0] * (l2 + 1) for _ in range(l1 + 1)] # +1 to allow for loops to hit base cases.
+        
+        for i in range(l1 - 1, -1, -1):
+            for j in range(l2 - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    tab[i][j] = 1 + tab[i + 1][j + 1]
+                else:
+                    tab[i][j] = max(tab[i + 1][j], tab[i][j + 1])
+        return tab[0][0]
+        
+        
+    def longestCommonSubsequence_TOPDOWN(self, text1: str, text2: str) -> int:
+        """
         PROBLEM:
         want to go through each string and look for the longest subsequence that are the same in both strings
         
@@ -17,9 +36,9 @@ class Solution:
         RECURRENCE RELATION:
         if there is a match, then LCS here is 1 + max(the LCS of incrementing i, the LCS from incrementing j)
         if there is no match, then 0 + ...
-        dp(i, j) = 0                                     <--- if i == len(text1) || j == len(text2)        
-        dp(i, j) = 1 + dp(i + 1, j + 1)   <--- if text1[i] == text2[j]
-        dp(i, j) = max(dp(i + 1, j), dp(i, j + 1))       <--- if text1[i] != text2[j]
+        dp(i, j) = 0                                <--- if i == len(text1) || j == len(text2)        
+        dp(i, j) = 1 + dp(i + 1, j + 1)             <--- if text1[i] == text2[j]
+        dp(i, j) = max(dp(i + 1, j), dp(i, j + 1))  <--- if text1[i] != text2[j]
         """
         memo = {}
         def dp(i, j):
