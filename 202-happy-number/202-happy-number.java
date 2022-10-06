1457b6684b08ddb6e1803ec1fcb5cf1beda2acbf
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-10-06 12:08:51 -0500

    Time: 3 ms (50.78%), Space: 41.3 MB (43.02%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -4,22 +4,25 @@ class Solution {
 
         */
         
-        Set<Integer> vis = new HashSet<>();
+        Set<Integer> seen = new HashSet<>();
         
         while (n != 1) {
-            if (vis.contains(n)) return false;
-            vis.add(n);
-            
-            int digitSum = 0;
-            while (n != 0) {
-                int digit = n % 10;
-                digitSum += Math.pow(digit, 2);
-                n /= 10;
-            }
-            
-            n = digitSum;
+            if (seen.contains(n)) return false;
+            seen.add(n);
+            n = sumDigitSquares(n);
         }
         
         return true;
+    }
+    
+    public int sumDigitSquares(int n) {
+        int digitSum = 0;
+        while (n != 0) {
+            int digit = n % 10;
+            digitSum += Math.pow(digit, 2);
+            n /= 10;
+        }
+        
+        return digitSum;
     }
 }
