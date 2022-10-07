a0b34b9c153d28940f1c8817c46e1be4579befde
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-10-06 23:13:14 -0500

    Time: 0 ms (100.00%), Space: 40.6 MB (89.11%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        /*
+            swap -> swapping two indices within the same string
+            doesn't matter which string you choose to perform the swap.
+            return true if you can make the strings equal by performing one swap (or none).
+            
+            exactly 2 unmatches (or 0)
+            those unmatches have to have the same letters.
+        */
+        
+        int diff1 = -1;
+        int diff2 = -1;
+        
+        for (int i = 0; i < s1.length(); ++i) {
+            char c1=s1.charAt(i), c2=s2.charAt(i);
+            if (c1 == c2) continue;
+
+            if (diff1 == -1) {
+                diff1 = i;
+            } else if (diff2 == -1) {
+                diff2 = i;
+            } else {
+                // more than 2 differences
+                return false;
+            }
+        }
+        
+        if (diff1 == -1 && diff2 == -1) return true;
+        if (diff1 == -1 || diff2 == -1) return false;
+        
+        // at this point, you will have the indices where each difference occurs.
+        // if the char in one string and one diff match the other char in the other diff
+        // and vice versa, everything is good.
+        return (s1.charAt(diff1) == s2.charAt(diff2)) && (s2.charAt(diff1) == s1.charAt(diff2));
+    }
+}
