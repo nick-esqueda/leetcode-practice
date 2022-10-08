dd8e619016f24a1ec932cf0f50d7211141bc02b9
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-10-07 22:40:08 -0500

    Time: 90 ms (26.92%), Space: 42.2 MB (95.41%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        
+        return bruteForce(nums, target);
+    }
+    
+    public int[] bruteForce(int[] nums, int target) {
         for (int i = 0; i < nums.length; ++i) {
-            int comp = target - nums[i];
-            
-            if (map.containsKey(comp)) return new int[] {map.get(comp), i};
-            map.put(nums[i], i);
+            for (int j = i + 1; j < nums.length; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
         }
         
-        return null;
+        return new int[0];
     }
 }
