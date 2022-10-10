f46da9f5049ed165c27daed42b32a6ca880a62cb
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-10-10 16:29:27 -0500

    Time: 16 ms (45.15%), Space: 45.8 MB (40.76%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,28 +1,8 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        /*
-        two pointers.
-        need to remember which letters that you've come across.
-        this will be tricky, because if one substr has repeating chars, it won't if you move L up by 1.
-        the dup might actually come from somewhere in the middle though...
-        
-        keep a set to remember which chars are in substr.
-        if R iterates forward and immediately finds a dup, then you have to:
-            move L up until that L dup is no longer in the substr,
-                (remove chars from set as move L up since no longer in substr)
-            then you can keep looping.
-        always take the length and compare to max.
-            
-        abcadef
-         i j
-         
-        a h b c d b e f
-        i         j
-        */
-        
+        int maxLen = 0;
         int L = 0;
         Set<Character> substr = new HashSet<>();
-        int maxLen = 0;
         
         for (int R = 0; R < s.length(); ++R) {
             char newChar = s.charAt(R);
