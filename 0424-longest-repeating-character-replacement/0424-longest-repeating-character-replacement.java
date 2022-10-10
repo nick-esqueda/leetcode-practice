dc72e95115c194c32ee4cc2b337eed0d2a677b28
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-10-10 17:48:58 -0500

    Time: 70 ms (15.67%), Space: 42.9 MB (72.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -3,29 +3,10 @@ class Solution {
         return twoPointer(s, k);
     }
     
-    public int twoPointer(String s, int k) {
-        /*
-            you have the length of the string...
-            use map with char counts?
-            you have the max char in the map, and the length...
-            if you subtract (length of str - max map char), that should be <= k.
-            the ratio of maxChar/len needs to stay the same (under k) to find the maxLen.
-            
-            after you add newChar to the map, check maxChar/len ratio.
-            if ratio is > k, need to:
-                move L up until the ratio is restored.
-                ? just continue in the loop for this?
-            
-            { a: 2, b: 6 }
-            A A B B B A B A B B B B     K=2
-                i             j
-            
-            len = 8
-        */
-        
+    public int twoPointer(String s, int k) {       
+        int maxLen = 0;
         int L = 0;
         Map<Character, Integer> counts = new HashMap<>();
-        int maxLen = 0;
         
         for (int R = 0; R < s.length(); ++R) {
             // put new char in the map.
