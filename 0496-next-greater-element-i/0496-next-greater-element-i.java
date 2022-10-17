ad13ab9a14b01c5ae1079af43651e61027120d52
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-10-17 17:06:59 -0500

    Time: 10 ms (24.12%), Space: 43.6 MB (79.95%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,9 +1,5 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        /*
-            
-        */
-        
         int[] ans = new int[nums1.length];
         
         for (int i = 0; i < nums1.length; ++i) {
