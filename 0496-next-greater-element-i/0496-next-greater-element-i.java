609efde84875745990b38c3230ef54934c1037a9
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-10-17 17:37:06 -0500

    Time: 4 ms (86.09%), Space: 44.1 MB (62.19%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,5 +1,35 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // return bruteForce(nums1, nums2);
+        return bruteForceOpt(nums1, nums2);
+    }
+    
+    public int[] bruteForceOpt(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> valMap = new HashMap<>();
+        for (int i = 0; i < nums2.length; ++i) {
+            valMap.put(nums2[i], i);           
+        }
+        
+        int[] res = new int[nums1.length];
+        
+        for (int i = 0; i < nums1.length; ++i) {
+            int target = nums1[i];
+            int targetIdx = valMap.get(target);
+            int nge = -1;
+            for (int j = targetIdx + 1; j < nums2.length; ++j) {
+                if (nums2[j] > target) {
+                    nge = nums2[j];
+                    break;
+                }
+            }
+            
+            res[i] = nge;
+        }
+        
+        return res;
+    }
+    
+    public int[] bruteForce(int[] nums1, int[] nums2) {
         int[] ans = new int[nums1.length];
         
         for (int i = 0; i < nums1.length; ++i) {
@@ -17,7 +47,6 @@ class Solution {
             
             ans[i] = nge;
         }
-        
         
         return ans;
     }
