c240413028d47e370014b65587ad0ca57ce7835e
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-10-17 17:00:33 -0500

    Time: 10 ms (24.12%), Space: 43.8 MB (71.73%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        /*
+            
+        */
+        
+        int[] ans = new int[nums1.length];
+        
+        for (int i = 0; i < nums1.length; ++i) {
+            int target = nums1[i];
+            int match = -1;
+            int nge = -1;
+            for (int j = 0; j < nums2.length; ++j) {
+                if (nums2[j] == target) match = j; // catch the start of the subprocess to find the NGE.
+                
+                if (match == -1) continue;
+                
+                if (nums2[j] > target) {
+                    nge = nums2[j];
+                    break;
+                }
+            }
+            
+            ans[i] = nge;
+        }
+        
+        
+        return ans;
+    }
+}
