4d7c6efb010bae5e60ded85c97e2c6c50fae3833
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-10-23 23:36:35 -0500

    Time: 2 ms (53.65%), Space: 43.8 MB (21.95%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,6 +15,36 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+        // return forLoop(root);
+        return whileLoop(root);
+    }
+    
+    public List<List<Integer>> whileLoop(TreeNode root) {
+       if (root == null) return new ArrayList<>();
+        
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            List<Integer> level = new ArrayList<>();
+            
+            while (len-- > 0) {
+                TreeNode curr = queue.poll();
+                level.add(curr.val);
+                
+                if (curr.left != null) queue.offer(curr.left);
+                if (curr.right != null) queue.offer(curr.right);
+            }
+            
+            res.add(level);
+        }
+        
+        return res; 
+    }
+    
+    public List<List<Integer>> forLoop(TreeNode root) {
         if (root == null) return new ArrayList<>();
         
         List<List<Integer>> res = new ArrayList<>();
