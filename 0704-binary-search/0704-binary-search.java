7db4d984e7fe5f5958a480fda8cbe65d63c4206f
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-11-01 22:06:14 -0500

    Time: 0 ms (100.00%), Space: 54.4 MB (34.80%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,9 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return v1(nums, target);
+        // return classic(nums, target);
+        return avoidOverflow(nums, target);
     }
     
-    public int v1(int[] nums, int target) {
+    public int avoidOverflow(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        
+        while (lo <= hi) {
+            // math trick to avoid integer overflow.
+            int mid = lo + ((hi - lo) / 2);
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public int classic(int[] nums, int target) {
         int lo = 0;
         int hi = nums.length - 1;
         
