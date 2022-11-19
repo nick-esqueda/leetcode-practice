26ad8f6485a0f31d82e9cfebc12d3f35a52e28c1
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-11-19 13:10:05 -0600

    Time: 139 ms (16.11%), Space: 14.5 MB (67.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,18 +3,11 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        s_map = dict()
-        t_map = dict()
+        s_map = defaultdict(int)
+        t_map = defaultdict(int)
 
         for i in range(len(s)):
-            s_char = s[i]
-            t_char = t[i]
-            if s_char not in s_map:
-                s_map[s_char] = 0
-            if t_char not in t_map:
-                t_map[t_char] = 0
-            
-            s_map[s_char] += 1
-            t_map[t_char] += 1
+            s_map[s[i]] += 1
+            t_map[t[i]] += 1
 
         return s_map == t_map
