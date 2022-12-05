5f830102dbae34d8ccae905ead65b4ca17bcf973
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-12-05 17:40:33 -0600

    Time: 807 ms (87.45%), Space: 71.7 MB (39.76%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -9,16 +9,38 @@ class TimeMap:
             
         self.hashmap[key].append((value, timestamp))
 
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.hashmap:
+    def get(self, key: str, timestamp: int) -> str: # O(log(n)) TIME
+        if key not in self.hashmap: 
             return ""
         
         vals = self.hashmap[key]
-        for val, time in reversed(vals):
-            if time <= timestamp:
-                return val
+        val, time = self.bin_search(vals, timestamp)
+        return val if time <= timestamp else ""
+    
+    def bin_search(self, vals, target_time):
+        lo = 0
+        hi = len(vals) - 1
+        while lo < hi:
+            mid = lo + ((hi - lo + 1) // 2) # right mid.
+            val, time = vals[mid]
+            
+            if time <= target_time:
+                lo = mid
+            else:
+                hi = mid - 1
         
-        return ""
+        return vals[lo]
+        
+#     def get(self, key: str, timestamp: int) -> str: # O(n) TIME
+#         if key not in self.hashmap:
+#             return ""
+        
+#         vals = self.hashmap[key]
+#         for val, time in reversed(vals):
+#             if time <= timestamp:
+#                 return val
+        
+#         return ""
 
 
 # Your TimeMap object will be instantiated and called as such:
