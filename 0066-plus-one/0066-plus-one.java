ceaf977e2a436ef7d2622396490fc9650ec76c15
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-12-14 13:00:04 -0600

    Time: 0 ms (100.00%), Space: 40.5 MB (97.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -10,22 +10,39 @@ class Solution {
         what if the carry over goes all the way? will need a new space at the beginning for the 1.
         */
         
-        int i = digits.length - 1;
+        return whileLoop(digits);
+        // return forLoop(digits);
+    }
+    
+    private int[] forLoop(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; --i) {            
+            ++digits[i];
+            if (digits[i] == 10) {
+                digits[i] = 0;
+            } else {
+                break;
+            }
+        }
+        
+        // if you break out and you still have a carry over, need a new array with 1 at the front.
+        return digits[0] == 0 ? bigCarry(digits) : digits;
+    }
+    
+    private int[] whileLoop(int[] digits) {
         boolean carry = true;
+        int i = digits.length - 1;
         while (i >= 0 && carry) {
             carry = false;
             
-            if (digits[i] == 9) {
+            ++digits[i];
+            if (digits[i] == 10) {
                 digits[i] = 0;
                 carry = true;
-            } else {
-                ++digits[i];
             }
             
             --i;
         }
         
-        // if you break out and you still have a carry over, need a new array with 1 at the front.
         return carry ? bigCarry(digits) : digits;
     }
     
