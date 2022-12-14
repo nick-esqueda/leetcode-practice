a561235014d069f3fc9e77b12ef15646696e8463
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-12-14 16:00:21 -0600

    Time: 22 ms (14.21%), Space: 39.3 MB (95.05%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,17 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        // the square root is the inverse of the square.
-        // square - this number multiplied by itself x times.
-        // square root - x number multiplied by itself 2 times.
-        // square root - what number multiplied by itself 2 times gets this number?
-        
-        // can iterate through all squares and find the last one underneath that number?
-        
+        return bruteForce(x);
+    }
+    
+    private int bruteForce(int x) {
         long num = 1;
         while (num * num <= x) {
             ++num;
         }
         
         return (int) num - 1;
+        
+        // THOUGHTS:
+        // the square root is the inverse of the square.
+        // square - this number multiplied by itself x times.
+        // square root - x number multiplied by itself 2 times.
+        // square root - what number multiplied by itself 2 times gets this number?
+        
+        // can iterate through all squares and find the last one underneath that number?
     }
 }
