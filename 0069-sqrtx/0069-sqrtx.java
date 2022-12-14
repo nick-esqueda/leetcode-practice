571dc074095b914768c8856da93e878f74ade029
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-12-14 17:48:32 -0600

    Time: 1 ms (100.00%), Space: 39.7 MB (86.69%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,28 @@
 class Solution {
     public int mySqrt(int x) {
-        return bruteForce(x);
+        // return bruteForce(x);
+        return binarySearch(x);
     }
+    
+    private int binarySearch(int x) {
+        // acceptable vals are <= x
+        // unacceptable vals are > x.
+        long lo = 0; 
+        long hi = x;
+        
+        while (lo < hi) {
+            long num = lo + ((hi - lo + 1) / 2); // right mid.
+            long square = num * num;
+
+            if (square <= x) {
+                lo = num;
+            } else {
+                hi = num - 1;
+            }
+        }
+        
+        return (int) lo;
+    }    
     
     private int bruteForce(int x) {
         long num = 1;
