221250c27becaa0b82d52bf354cf3fc19943f19f
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-12-15 11:13:03 -0600

    Time: 749 ms (5.01%), Space: 45.5 MB (84.21%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return bruteForce(headA, headB);
+    }
+    
+    private ListNode bruteForce(ListNode headA, ListNode headB) {
+        /*
+        for each node in listA, traverse through listB to see if a node matches.
+        */
+        
+        ListNode currA = headA;
+        
+        while (currA != null) {
+            ListNode currB = headB;
+            while (currB != null) {
+                if (currB == currA) {
+                    return currB;
+                }
+                currB = currB.next;
+            }
+            
+            currA = currA.next;
+        }
+        
+        return null;
+    }
+    
+    
+}
