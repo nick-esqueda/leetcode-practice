e34c0a8dbfd7093bf284bca0755057864e24d5ac
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2022-12-15 06:24:58 -0600

    Time: 0 ms (100.00%), Space: 42.8 MB (58.31%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,21 +1,5 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int insertionPoint = nums1.length - 1;
-        int idx1 = m - 1;
-        int idx2 = n - 1;
-        
-        while (idx1 >= 0 || idx2 >= 0) {
-            if (idx1 < 0) {
-                nums1[insertionPoint--] = nums2[idx2--];
-            } else if (idx2 < 0) {
-                nums1[insertionPoint--] = nums1[idx1--];
-            } else if (nums1[idx1] >= nums2[idx2]) {
-                nums1[insertionPoint--] = nums1[idx1--];
-            } else {
-                nums1[insertionPoint--] = nums2[idx2--];
-            }
-        }
-        
         /*
         THOUGHTS:
         the greatest ele's to the back, so....
@@ -45,5 +29,43 @@ class Solution {
                   b
         */
         
+        threePointersV1(nums1, m, nums2, n);
+    }
+    
+    private void threePointersV2(int[] nums1, int m, int[] nums2, int n) {
+        int insertionPoint = nums1.length - 1;
+        int idx1 = m - 1;
+        int idx2 = n - 1;
+        
+        while (idx1 >= 0 || idx2 >= 0) {     
+            if (idx2 < 0) {
+                return;
+            }
+            
+            // idx2 is always in-bounds here, so idx1 could be anything.
+            if (idx1 >= 0 && nums1[idx1] >= nums2[idx2]) {
+                nums1[insertionPoint--] = nums1[idx1--];
+            } else {
+                nums1[insertionPoint--] = nums2[idx2--];
+            }
+        }
+    }
+    
+    private void threePointersV1(int[] nums1, int m, int[] nums2, int n) {
+        int insertionPoint = nums1.length - 1;
+        int idx1 = m - 1;
+        int idx2 = n - 1;
+        
+        while (idx1 >= 0 || idx2 >= 0) {
+            if (idx1 < 0) {
+                nums1[insertionPoint--] = nums2[idx2--];
+            } else if (idx2 < 0) {
+                nums1[insertionPoint--] = nums1[idx1--];
+            } else if (nums1[idx1] >= nums2[idx2]) {
+                nums1[insertionPoint--] = nums1[idx1--];
+            } else {
+                nums1[insertionPoint--] = nums2[idx2--];
+            }
+        }
     }
 }
