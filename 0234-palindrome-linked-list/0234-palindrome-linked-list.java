9ec4e06903118c9f0755a9204d8b9696eef6225a
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2023-04-08 18:37:22 -0500

    Time: 6 ms (46.29%), Space: 63.3 MB (23.62%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,29 +10,87 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> orderedListValues = new ArrayList<>();
-        
-        // collect node values in an ordered list.
-        ListNode currentNode = head;
-        while (currentNode != null) {
-            orderedListValues.add(currentNode.val);
-            currentNode = currentNode.next;
+        // get the length of the list so that we can split it in half.
+        int listLength = getListLength(head);
+        // if list only has one element, it is a palindrome.
+        if (listLength == 1) {
+            return true;
         }
 
-        // iterate over linked list forwards, iterate over ordered list backwards,
-        // if not equal during an iteration, then list is not palindrome.
-        currentNode = head;
-        for (int i = orderedListValues.size() - 1; i >= 0; --i) {
-            int val1 = orderedListValues.get(i);
-            int val2 = currentNode.val;
-            
-            if (val1 != val2) {
+        // split the list in half, and get the head of each half.
+        ListNode[] listHalves = splitListInHalf(head, listLength);
+        ListNode firstHalf = listHalves[0];
+        ListNode secondHalf = listHalves[1];
+        
+        // now that the two lists are disconnected, reverse one half in-place.
+        ListNode firstHalfReversed = reverseLinkedListInPlace(firstHalf);
+        
+        // iterate through both lists and check equality on each step through. 
+        // based on this, return the boolean.
+        return areListsSame(firstHalfReversed, secondHalf);
+    }
+    
+    public boolean areListsSame(ListNode headA, ListNode headB) {
+        ListNode currentA = headA;
+        ListNode currentB = headB;
+        
+        while ((currentA != null) && (currentB != null)) {
+            if (currentA.val != currentB.val) {
                 return false;
             }
             
-            currentNode = currentNode.next;
+            currentA = currentA.next;
+            currentB = currentB.next;
         }
         
         return true;
     }
+
+    
+    public ListNode reverseLinkedListInPlace(ListNode head) {        
+        ListNode currentNode = head;
+        ListNode previousNode = null;
+        while (currentNode != null) {
+            ListNode nextNode = currentNode.next;
+            
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+        
+        return previousNode;
+    }
+        
+    public ListNode[] splitListInHalf(ListNode head, int listLength) {
+        int index = 0;
+        int halfwayPoint = listLength / 2; // floor/integer division
+        ListNode previousNode = null;
+        ListNode currentNode = head;
+        while (index < halfwayPoint) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+            index += 1;
+        }
+
+        // if odd length, then you can exclude the middle node.
+        if (listLength % 2 == 1) {
+            currentNode = currentNode.next;
+        }
+
+        previousNode.next = null; // disconnect the two lists.
+
+        return new ListNode[] { head, currentNode };
+    }
+    
+    public int getListLength(ListNode head) {
+        int listLength = 0;
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            listLength += 1;
+            currentNode = currentNode.next;
+        }
+        
+        return listLength;
+    }
+
 }
