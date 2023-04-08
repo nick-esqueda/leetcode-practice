e70ce8d5dfb35f6b671af6588fb78feb7e093d32
Nick Esqueda <93935486+nick-esqueda@users.noreply.github.com>
2023-04-07 22:17:55 -0500

    Time: 6 ms (88.60%), Space: 42.9 MB (64.67%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,6 +1,24 @@
 class Solution {
     public int firstUniqChar(String s) {
-        return hashMap(s);
+        // return hashMap(s);
+        return arrayCounter(s);
+    }
+    
+    public int arrayCounter(String s) {
+        int[] counter = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            counter[c - 'a'] += 1;
+        }
+        
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (counter[c - 'a'] == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
     }
     
     public int hashMap(String s) {
